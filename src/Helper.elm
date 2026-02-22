ec3d2133ac8e60625aed65bec0330563cb182c4e
Polo-Najera <up230328@alumnos.upa.edu.mx>
2026-02-22 16:08:38 -0600

    homework 5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,51 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Pending
+    | Failed
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map
+        (\grade ->
+            if grade >= 7 then
+                Approved
+
+            else if grade < 0 then
+                Pending
+
+            else
+                Failed
+        )
+        grades
+
+
+type AirplaneStatus
+    = OnTime
+    | Delayed
+    | Boarding
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        OnTime ->
+            "Esperar"
+
+        Delayed ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
